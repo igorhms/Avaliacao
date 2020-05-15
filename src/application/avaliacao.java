7b10aed5064091d841cb2aaa6a7d7fcf4ce6ac24
Igor Machado <igorhms98@gmail.com>
2020-05-15 19:56:58 -0300

    Cabecalho

--- src/application/avaliacao.java
+++ src/application/avaliacao.java
@@ -1,10 +1,21 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 class avaliacao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("-----------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.println();
+		
+		sc.close();
 	}
 
 }
